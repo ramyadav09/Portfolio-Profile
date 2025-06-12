c747ad481ea60d9066cd984613afeeef2c953a61
Ram Kumar Yadav <145370048+ramyadav09@users.noreply.github.com>
2025-06-12 19:06:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,28 +6,76 @@ hamburger.addEventListener("click", () => {
   cross.classList.remove("-translate-y-96");
   cross.classList.add("-translate-y-12");
   menu_list.classList.toggle("-translate-x-full");
-  console.log("i am clicked");
+  // console.log("i am clicked");
 });
+
 cross.addEventListener("click", () => {
   hamburger.classList.toggle("-translate-x-96");
   cross.classList.remove("-translate-y-12");
   cross.classList.add("-translate-y-96");
   menu_list.classList.toggle("-translate-x-full");
-  console.log("i am clicked");
+  // console.log("i am clicked");
 });
+
+//lists effects
+
+let home = document.querySelector(".home");
+let about = document.querySelector(".about");
+let skills = document.querySelector(".skills");
+let portfolio = document.querySelector(".portfolio");
+let contact = document.querySelector(".contact");
 let lists = document.querySelectorAll(".lists");
-lists.forEach((click) => {
-  click.addEventListener("click", () => {
+lists.forEach((Event1) => {
+  Event1.addEventListener("click", () => {
     hamburger.classList.toggle("-translate-x-96");
     cross.classList.remove("-translate-y-12");
     cross.classList.add("-translate-y-96");
     menu_list.classList.toggle("-translate-x-full");
-    // console.log("i am clicked");
+    lists.forEach((el) => {
+      el.classList.remove("text-purple-300");
+    });
+    Event1.classList.add("text-purple-300");
+    console.log("i am clicked");
   });
 });
+function homefun() {
+  lists.forEach((el) => {
+    el.classList.remove("text-purple-300");
+  });
+  home.classList.add("text-purple-300");
+}
+function aboutfun() {
+  lists.forEach((el) => {
+    el.classList.remove("text-purple-300");
+  });
+  about.classList.add("text-purple-300");
+}
+function skillsfun() {
+  lists.forEach((el) => {
+    el.classList.remove("text-purple-300");
+  });
+  skills.classList.add("text-purple-300");
+}
+function portfoliofun() {
+  lists.forEach((el) => {
+    el.classList.remove("text-purple-300");
+  });
+  portfolio.classList.add("text-purple-300");
+}
+function contactfun() {
+  lists.forEach((el) => {
+    el.classList.remove("text-purple-300");
+  });
+  contact.classList.add("text-purple-300");
+}
 
 var typed = new Typed(".web-dev", {
-  strings: ["Web Developer", "Student"],
+  strings: [
+    "Frontend Developer",
+    "Web Designer",
+    "JavaScript Enthusiast",
+    "CSE Student",
+  ],
   typeSpeed: 50,
   backSpeed: 50,
   backDelay: 500,
